f1eaf36bbf3eb30f2079f22179ddafa8a4c6a38b
ChanIlKang <programmer.chanil@gmail.com>
2024-01-05 11:19:18 +0900

    refactor some code, change menu, add the kakaomap API

--- js/index.js
+++ js/index.js
@@ -11,30 +11,22 @@ const nav = document.querySelectorAll(".kb_nav_container");
 
 let currentView = 1;
 
-const menuText = `폭찹스테이크
-양송이토마토파스타
-감자고로케
-속풀이 김치콩나물국
-제철 물미역줄기볶음
-숙주나물과 오이피클
-야채샐러드
+const menuText = `돼지불고기와 상추쌈
+순살돈까스
+웰빙카레
+햄감자볶음
+건새우아욱된장국
+시금치나물
+야채샐러드 과일 토스트 커피
 건강흑미밥과 햅쌀밥
-배추김치와 각종반찬
-과일 토스트 커피`;
+배추김치와 각종반찬`;
 
-const basicInfo = `강남밥상 남부터미널점
-남부터미널역(1번출구) 깐부치킨 퓨전캐쥬얼뷔페
-이용시간: 조식특선 월~토 06:30~08:30
-런치뷔페 월~토 11:00~14:00
-이용요금: 1인이용시 현금 8,500원 / 카드 9,000원 식권 10장 현금 75,000원 / 카드 85,000원
-위치: 남부터미널역 1번출구/ 서초중앙로42 블루핀타워`
-
+// 위치: 남부터미널역 1번출구/ 서초중앙로42 블루핀타워
 //time
 dateElement.textContent = `${year}년 ${month}월 ${day}일`;
 
 //txt data
 setMenu.innerText = menuText;
-billing.innerText = basicInfo;
 
 //functions
 checkScreen();
@@ -57,3 +49,21 @@ function checkScreen() {
         }
     }
 }
+
+//kakao map
+const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+let options = { //지도를 생성할 때 필요한 기본 옵션
+    center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+    level: 3 //지도의 레벨(확대, 축소 정도)
+};
+
+let map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+var markerPosition  = new kakao.maps.LatLng(33.450701, 126.570667);
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
